17e4bd7ee8e3c2c1f882633723e94d9bf7e0a931
Martijn Engler <git@martijnengler.nl>
2012-10-15 23:42:26 +0200

    removed the preventDefault that was there for debug reasons

--- disable-submit.js
+++ disable-submit.js
@@ -2,9 +2,9 @@ jQuery(function($){
 	$(".gform_wrapper form").on("submit", function(e){
 		b = $("input[type=submit]", this);
 		b.attr("disabled", true);
-		b.css("background", "darkgray"); 
-		b.css("color", "black"); 
-		b.val("Verwerken…");		
-		e.preventDefault();
+		b.css("background", "darkgray");
+		b.css("color", "black");
+		b.css("width", b.width() + "px");
+		b.val("Verwerken…");
 	});
 });
